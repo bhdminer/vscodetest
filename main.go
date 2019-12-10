4ee50c401b218d765148eb7d608c587802e135f5
bhdminer <bhdminer@gmail.com>
2019-12-10 11:40:39 -0800

    ok 4x

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello kitty, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello kitty xxx, %s!", r.URL.Path[1:])
 }
